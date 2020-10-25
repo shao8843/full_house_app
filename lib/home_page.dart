7766047c16f2a26ad66eb7de5dccbc8923e6b211
Charles <charles@cognitivateleap.com>
2020-10-25 10:57:00 -0700

    Added home page

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  const HomePage():super();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
