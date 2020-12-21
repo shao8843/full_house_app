791112ca52004a26dab2f14806d0dc336d633080
Charles Shao <shao8843@yahoo.com>
2020-12-21 12:56:56 -0800

    Implemented pushUserEditPage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter_artech/flutter_artech.dart';
 import 'package:flutter/material.dart';
+import 'package:full_house_app/pages/user_edit_page.dart';
+import 'package:full_house_app/user/me_data.dart';
 
-class LoginPage extends LoginPageBase {
+class LoginPage extends LoginPageBase<MeData> {
 
   const LoginPage({Key key}) :super(key: key);
+
+  @override
+  Future<MeData> pushUserEditPage(BuildContext context) async {
+    return await Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => UserEditPage()
+    ));
+  }
 }
